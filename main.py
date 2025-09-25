eab07445983b7b60734601074cbf6466b039b621
Grischmeister <grischa.stass@gmx.de>
2025-09-25 13:05:01 +0200

    added timer to measure execution time

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 # main.py
+import time
+
 from experiments import heads_up_las_vegas
 from src.deck import create_deck
 from src.experiments import heads_up_monte_carlo
 
 def main():
+    start = time.time()
     deck = create_deck()
 #    heads_up_monte_carlo(100000, deck)
     heads_up_las_vegas(deck)
+    end = time.time()
+    print(end - start)
 
 if __name__ == "__main__":
     main()
